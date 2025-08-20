934bed46e07245b1f95302a0d1e5e345fb8c37dc
user-of-name <belte.francis@gmail.com>
2025-08-20 14:37:49 +0300

    remove stalls.html and update stalls.php with new exhibition content and styling

---
+++ Projects/stalls.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php 
+$title = "Stalls";
+include '../includes/head.php';
+?>
+
+<body>
+<div class="container">
+  <h1 class="text-center">✨(っ◔︣◡◔᷅)っ** Poing exhibition **c(◕︣◡◕᷅c)✨</h1>
+</div>
+<?php include '../includes/navbar.php'; ?>
+<!-- first row of columns -->
+<div class="row" style="width: 95%; margin: 0 auto;">
+    <div class="col-md-4">
+    <p style="padding: 10px;">
+<p>
+All members of our collective can contribute when organizing a stall.
+<br>Since we are a collective of zine makers, screenprinters, tattooers and general art print makers
+<br>we have lots of affordable goodies to sell, we are open for trading and donations.
+<br>We apply multiple times a year for stalls in fairs that we like :)
+<br>If you can't manage to come to the stalls, you can buy our some of our things in our <a href="https://www.etsy.com/shop/UnitedFriedFrontShop">Etsy shop</a>
+</p>
+    </div>
+    <div class="col-md-8">
+        <a>
+            <div style="width: 95%;">
+                <img src="/Images/anarchist book fair 1.png" class="project-img">
+                <img src="/Images/anarchist book fair 2.png" class="project-img">
+                <img src="/Images/anarchist book fair 3.png" class="project-img">
+                <img src="https://64.media.tumblr.com/0a4bd6c5e6523c92a12aa61966be794f/88af7a8ef00f9525-92/s640x960/1e5af3e32090093aeb7ed38e248d87943ec0eb9a.jpg" class="project-img">
+                <img src="https://64.media.tumblr.com/da8a67b4bf20d86037007f965a3a3832/e5c5f05a360494d0-80/s640x960/e01c6be44a004718b7bdb47dab5819e20e20cf2a.jpg" class="project-img"> 
+            </div>
+        </a>
+    </div>
+</div>
